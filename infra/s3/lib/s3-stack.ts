61ad686efb8102b059d8ecb9ac29dfb801c3bd0a
pikka1999 <h_tanaka@toridori.co.jp>
2024-09-20 15:15:36 +0900

    s3リソースを作れるようにする

---
+++ infra/s3/lib/s3-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class S3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'remix-sandbox-bucket',
+      versioned: false,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+    });
+  }
+}
